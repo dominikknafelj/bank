a9197482e778ea9bde95a0b09a4939b54202b5ab
Dominik Knafelj <d.knafelj@gmail.com>
2016-04-25 21:02:31 -0400

    Implemented the Konto class

--- src/bank/Konto.java
+++ src/bank/Konto.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bank;
 
 /**
@@ -10,5 +5,75 @@ package bank;
  * @author Dominik Knafelj <d.knafelj@gmail.com>
  */
 public class Konto {
+    private long kontostand;
+    private long nummer;
+    private Kunde inhaber = null;
+    private Bank bank = null;
     
+    /**
+     * erzeugt eine Konto mit entsprechender Kontonummer, Bank und Kunden
+     * @param ktoNummer - die Kontonummer
+     * @param b - die Bank
+     * @param k - der Kunde
+     */
+    public Konto(long ktoNummer, Bank b, Kunde k){
+        this.nummer = ktoNummer;
+        this.bank = b;
+        this.inhaber = k;
+        this.kontostand = 0;
+    }
+    
+    /**
+     * reduziert den Kontostand um den entsprechenden centwert
+     * @param cent - der centwert
+     */
+    public void auszahlen(long cent)
+    {
+        this.kontostand-=cent;
+    }
+    
+    /**
+     * erhoeht den Kontostand um den entsprechenden centwert
+     * @param cent - der centwert
+     */
+    public void einzahlen(long cent)
+    {
+        this.kontostand+=cent;
+    }
+    
+    /**
+     * gibt den Banknamen der assoziierten Bank zurueck
+     * @return den Banknamen
+     */
+    public String liefereBankname(){
+        return this.bank.liefereName();
+    }
+    /**
+     * gibt die BLZ der assoziierten Bank zuruek
+     * @return - die BLZ
+     */
+    public long liefereBLZ(){
+        return this.bank.liefereBLZ();
+    }
+    /**
+     * gibt den Namen den Inhabers zurueck
+     * @return der Inhabername
+     */
+    public String liefereInhabername(){
+        return this.inhaber.liefereName();
+    }
+    /**
+     * gibt die Kontonummer zurueck
+     * @return die Kontonummer
+     */
+    long liefereKontonummer(){
+        return this.nummer;
+    }
+    /**
+     * gibt den Kontostand zurueck
+     * @return der Kontostand
+     */
+    long liefereKontostand(){
+        return this.kontostand;
+    }
 }
